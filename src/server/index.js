f54a522a1a10f9b4048acf4a866228ffe98fdba0
Joe Novak <obey-hackintosh@Joes-Mac-Pro.local>
2019-04-07 13:29:05 -0400

    Setting Up IO Framework

--- src/server/index.js
+++ src/server/index.js
@@ -10,7 +10,35 @@ app.use(express.static(path.join(__dirname, '../../build')));
 app.get('/', (req, res, next) =>
     res.sendFile(__dirname + './index.html'));
 
-io.on('connection', socket => 
-    socket.emit('hello', {message: 'hello from server!'}));
+io.on('connection', (client) => {
+    console.log(client.id + ' Connected');
+
+    client.on('penDown', (coordinates) => {
+        io.emit('ioPenDown', coordinates);
+    })
+
+    client.on('hideCanvas', (state) => {
+        state ? client.broadcast.emit('ioHideCanvas') : client.broadcast.emit('ioShowCanvas');
+    })
+
+    client.on('showCanvas', () => {
+    })
+
+    client.on('drawing', (coordinates) => {
+        io.emit('drawing', coordinates);
+    })
+
+    client.on('penUp', () => {
+        io.emit('ioPenUp');
+    })
+
+    client.on('clear', () => {
+        io.emit('clear');
+    })
+
+    client.on('setDimensions', (dimensions) => {
+        io.emit('ioSetDimensions', dimensions)
+    })
+});
 
 server.listen(port);
