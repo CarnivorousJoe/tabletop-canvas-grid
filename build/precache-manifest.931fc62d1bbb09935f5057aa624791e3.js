3ead79a625b82f6bccf59c28604ad7cb27f39111
Joe Novak <obey-hackintosh@Joes-Mac-Pro.local>
2019-04-07 13:25:15 -0400

    Build for heroku

---
+++ build/precache-manifest.931fc62d1bbb09935f5057aa624791e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "51a89b6f17ba0bfe0c4f",
+    "url": "/static/css/main.5df8135a.chunk.css"
+  },
+  {
+    "revision": "51a89b6f17ba0bfe0c4f",
+    "url": "/static/js/main.5073d263.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "dcb04149d6781fd887f5",
+    "url": "/static/js/2.0d5ca911.chunk.js"
+  },
+  {
+    "revision": "eb37a46dd7f6e5a8ca0a48c7b59463d7",
+    "url": "/index.html"
+  }
+];
--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { socket } from './api/Socketio';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+socket.on('hello', ({ message }) => alert(message));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
